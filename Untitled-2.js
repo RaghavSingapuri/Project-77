294ef859af329b8e20a917f61aaa81172ca650cd
Raghav Singapuri <92780917+RaghavSingapuri@users.noreply.github.com>
2022-04-28 17:53:45 +0530

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,27 @@
+<html>
+<Head>
+<title> Project 77</title>
+</Head>
+<body>
+<script>
+    console.log("My first console Project ");
+
+    var first_name = "Ranbir";
+    console.log(first_name);
+
+    var last_name = "kapoor";
+    console.log(last_name);
+
+    var Full_name = first_name.concat(last_name) ;
+    console.log(Full_name);
+    
+    var = 10 ;
+    var = 5 ;
+    var add = 10 +5 ;
+    console.log(add) ;
+
+</script>
+
+</body>
+
+</html>
